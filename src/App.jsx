1fc08a308554536993d203dbb11335ddc25f87c8
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2023-10-15 18:17:45 +0530

    install react js and tailwind css and create first template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="text-red-400">home</p>
+    </>
+  );
+}
+export default App;
