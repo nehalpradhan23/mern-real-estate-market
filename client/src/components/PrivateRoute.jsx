93e8765f0953568c58eccdd93df2867d21ce2b4a
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2023-10-30 22:21:42 +0530

    complete profile page UI

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -3,5 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export default function PrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
+  // goto child component using outlet
   return currentUser ? <Outlet /> : <Navigate to={"/sign-in"} />;
 }
