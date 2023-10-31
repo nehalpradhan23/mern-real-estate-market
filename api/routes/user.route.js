073bb47ed361da43c72c658d2537795489c63d80
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2023-10-31 19:49:09 +0530

    create user update API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+// API to update user profile
+// update after verifying user
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
