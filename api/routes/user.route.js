c34921cc3015fc90ede5f01445f017127a641cbf
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2023-11-04 21:03:33 +0530

    create get user listings API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   test,
   updateUser,
   deleteUser,
+  getUserListing,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -13,5 +14,6 @@ router.get("/test", test);
 // update after verifying user
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListing);
 
 export default router;
