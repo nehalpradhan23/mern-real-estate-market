303a337f422ba0221b7929247bd433d8bed9783b
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2023-11-09 23:37:11 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About My Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        This is a fully functioning demo project created for practise during my
+        learning phase of ReactJS.
+      </p>
+      <p className="mb-4 text-slate-700">
+        This project is about a real estate website where user can discover, or
+        have their own real estate property for selling or renting
+      </p>
+      <p className="mb-4 text-slate-700">
+        This project is made completely from scratch following tutorials and
+        online guides using ReactJS and tailwind CSS along with mongoDB,
+        firebase and Redux
+      </p>
+    </div>
+  );
 }
